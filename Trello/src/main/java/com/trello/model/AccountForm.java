d8fee563ac822640809903e03d68b3c38b81d5d3
Lukasz <sanjuro20@o2.pl>
2015-11-18 19:02:25 +0100

    walidacja uzytkownika fixed

--- Trello/src/main/java/com/trello/model/AccountForm.java
+++ Trello/src/main/java/com/trello/model/AccountForm.java
@@ -1,10 +1,21 @@
 package com.trello.model;
 
+import javax.validation.constraints.AssertTrue;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import org.apache.commons.lang.builder.ToStringBuilder;
 import org.apache.commons.lang.builder.ToStringStyle;
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.ScriptAssert;
+
+@ScriptAssert(
+		lang = "javascript",
+		script = "_this.confirmPassword.equals(_this.password)",
+		message = "account.password.mismatch.message")
 
 public class AccountForm {
-	
+		
 	//Dane osobowe
 	private String username;
 	private String password;
@@ -18,65 +29,91 @@ public class AccountForm {
 	//Potwierdzenie umowy
 	private boolean acceptTerms = false;
 	
+	
+	
+	//=======Gettery========//
+	@NotNull
+	@Size(min = 1, max = 50)
 	public String getUsername(){
 		return username;
 	}
 	
+	
+	@NotNull
+	@Size(min = 1, max = 50)
+	public String getPassword() {
+		return password;
+	}
+	
+	@NotNull
+	@Size(min = 1, max = 50)
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	@NotNull
+	@Size(min = 1, max = 50)
+	public String getLastName() {
+		return lastName;
+	}
+	
+	@NotNull
+	@Size(min = 1, max = 50)
+	@Email
+	public String getEmail() {
+		return email;
+	}
+	
+	@NotNull
+	@Size(min = 1, max = 50)
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}	
+	
+	public boolean isMarketingOk() {
+		return marketingOk;
+	}
+	
+	
+	@AssertTrue(message = "{account.acceptTerms.assertTrue.message}")
+	public boolean isAcceptTerms() {
+		return acceptTerms;
+	}
+	//========Settery=======//
 	public void setUsername(String username){
 		this.username = username;
 	}
 
-	public String getPassword() {
-		return password;
-	}
 
 	public void setPassword(String password) {
 		this.password = password;
 	}
 
-	public String getConfirmPassword() {
-		return confirmPassword;
-	}
 
 	public void setConfirmPassword(String confirmPassword) {
 		this.confirmPassword = confirmPassword;
 	}
 
-	public String getFirstName() {
-		return firstName;
-	}
 
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
 
-	public String getLastName() {
-		return lastName;
-	}
-
+	
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
 
-	public String getEmail() {
-		return email;
-	}
 
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
-	public boolean isMarketingOk() {
-		return marketingOk;
-	}
 
 	public void setMarketingOk(boolean marketingOk) {
 		this.marketingOk = marketingOk;
 	}
 
-	public boolean isAcceptTerms() {
-		return acceptTerms;
-	}
 
 	public void setAcceptTerms(boolean acceptTerms) {
 		this.acceptTerms = acceptTerms;
